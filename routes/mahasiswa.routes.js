fd051787334b15ea15c45bc50081739b6d860c18
farukgeming323-dotcom <farukgeming323@gmail.com>
2026-06-03 11:57:41 +0700

    Connecting to github

---
+++ routes/mahasiswa.routes.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import {
+    getAllMahasiswa,
+    tambahmahasiswabaru,
+    cariMahasiswaByNIM,
+    updateMahasiswa,
+    deleteMahasiswa
+} from "../controllers/mahasiswa.controllers.js";
+
+const router = express.Router();
+
+router.get("/", getAllMahasiswa);
+router.post("/", tambahmahasiswabaru);
+router.get("/:id", cariMahasiswaByNIM);
+router.patch("/:id", updateMahasiswa);
+router.delete("/:id", deleteMahasiswa);
+
+export default router;
