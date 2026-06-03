84c0926b76740b0c8800d1f69627fe5902ae9323
parkkruk969 <farukgeming323@gmail.com>
2026-06-03 11:34:05 +0700

    Add root endpoint with welcome message

--- bukumahapro/index.js
+++ bukumahapro/index.js
@@ -20,6 +20,9 @@ try {
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.get('/',(req,res)=>{
+res.json({message:"Hello coba backend untuk vercel"});
+});
 app.use('/api/buku',bukus);
 app.use('/api/mahasiswa',mahasiswas);
 app.use('/api/prodi',prodis);
